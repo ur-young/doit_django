ecce0d6ce3a71d224bf715ce177eb6905ae084fd
DESKTOP-K5ML059\user <you_r_young@naver.com>
2022-10-26 17:03:12 +0900

    FBV blog post subpage created

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import index
+from . import views
 
 urlpatterns = [
     path('', index, name="index"),
+    path('<int:pk>/', views.single_post_page),
+    path('', views.index),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,15 +5,21 @@ from .models import Post
 def index(request):
     posts = Post.objects.all().order_by('-pk')
 
-    # 필요한 코드 넣기
-    data = "블로그데이터"
-    # render(request, '템플릿 파일', {'키' : 변수값})의 형태
-    # 템플릿에서는 '키'가 변수가 되는 것임
     return render(
         request, 
         'blog/index.html', 
         {
-            'datas' : data, 
             'posts' : posts
+        }
+    )
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {
+            'post' : post,
         }
     )
