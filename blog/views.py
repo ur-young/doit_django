cd98f28ddf7e3d02e83b9d98dba8792c0dc114c9
DESKTOP-K5ML059\user <you_r_young@naver.com>
2022-10-26 16:12:55 +0900

    FBV blog post list page created

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def index(request):
+    posts = Post.objects.all().order_by('-pk')
+
     # 필요한 코드 넣기
     data = "블로그데이터"
     # render(request, '템플릿 파일', {'키' : 변수값})의 형태
     # 템플릿에서는 '키'가 변수가 되는 것임
-    return render(request, 'blog/index.html', {'datas' : data})
+    return render(
+        request, 
+        'blog/index.html', 
+        {
+            'datas' : data, 
+            'posts' : posts
+        }
+    )
