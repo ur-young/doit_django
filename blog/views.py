ce9b337a0cd816be42ce9d5a2e6bb851d65fb357
DESKTOP-K5ML059\user <you_r_young@naver.com>
2022-10-26 15:57:57 +0900

    FBV page created

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    # 필요한 코드 넣기
+    data = "블로그데이터"
+    # render(request, '템플릿 파일', {'키' : 변수값})의 형태
+    # 템플릿에서는 '키'가 변수가 되는 것임
+    return render(request, 'blog/index.html', {'datas' : data})
--- doit_django_prj/urls.py
+++ doit_django_prj/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from xml.etree.ElementInclude import include 있으면 오류나니까 주석처리
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls'))
 ]
