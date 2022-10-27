bb08b4542c972665277f1566b80f686d284df8be
DESKTOP-K5ML059\user <you_r_young@naver.com>
2022-10-27 13:49:32 +0900

    thumbnail added to post_list

--- doit_django_prj/settings.py
+++ doit_django_prj/settings.py
@@ -124,6 +124,9 @@ USE_TZ = False
 STATIC_URL = 'static/'
 STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
