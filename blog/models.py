0166cee0bf56c85e400ed841c7bf964bee8e138b
DESKTOP-K5ML059\user <you_r_young@naver.com>
2022-10-28 11:15:21 +0900

    add category function on post_list

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
+
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
@@ -16,6 +28,8 @@ class Post(models.Model):
     
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+
     def __str__(self):
         return f'[{self.pk}] {self.title} :: {self.author}'
 
@@ -27,3 +41,4 @@ class Post(models.Model):
 
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
+
